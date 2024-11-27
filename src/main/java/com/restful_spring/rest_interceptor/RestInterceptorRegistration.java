923951c4be23dfb4580505141898db494a1af05f
Kim Daehyeon <daehyeon3351@gmail.com>
2024-11-28 07:53:26 +0900

    docs: add comments

--- src/main/java/com/restful_spring/rest_interceptor/RestInterceptorRegistration.java
+++ src/main/java/com/restful_spring/rest_interceptor/RestInterceptorRegistration.java
@@ -4,24 +4,42 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Assists with the creation of a {@link RestInterceptor}.
+ *
+ * @author Dh3356
+ * @since 0.1
+ */
 public class RestInterceptorRegistration {
 
     private final RestInterceptor restInterceptor;
     private int order = 0;
 
+    /**
+     * Creates a new instance of {@link RestInterceptorRegistration}.
+     */
     public RestInterceptorRegistration(RestInterceptor restInterceptor) {
         this.restInterceptor = restInterceptor;
     }
 
+    /**
+     * Add RestfulPatterns the interceptor should be included in.
+     */
     public RestInterceptorRegistration addRestfulPatterns(RestfulPattern... restfulPatterns) {
         return addRestfulPatterns(List.of(restfulPatterns));
     }
 
+    /**
+     * List-based variant of {@link #addRestfulPatterns(RestfulPattern...)}.
+     */
     public RestInterceptorRegistration addRestfulPatterns(Collection<RestfulPattern> restfulPatterns) {
         restInterceptor.restfulPatterns = new ArrayList<>(restfulPatterns);
         return this;
     }
 
+    /**
+     * Specify an order position to be used. Default is 0.
+     */
     public RestInterceptorRegistration order(int order) {
         this.order = order;
         return this;
--- src/main/java/com/restful_spring/rest_interceptor/RestInterceptorRegistry.java
+++ src/main/java/com/restful_spring/rest_interceptor/RestInterceptorRegistry.java
@@ -5,6 +5,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
+/**
+ * Helps with configuring a list of RestInterceptors. Adaptor between InterceptorRegistry and RestInterceptor.
+ *
+ * @author Dh3356
+ * @since 0.1
+ */
 public class RestInterceptorRegistry {
 
     private final InterceptorRegistry registry;
@@ -14,12 +20,23 @@ public class RestInterceptorRegistry {
         this.registry = registry;
     }
 
+    /**
+     * Adds the provided {@link RestInterceptor}.
+     *
+     * @param restInterceptor the restInterceptor to add
+     * @return an {@link RestInterceptorRegistration} that allows you optionally configure the registered
+     * restInterceptor further for example adding RestfulPatterns it should apply to.
+     */
     public RestInterceptorRegistration addInterceptor(RestInterceptor restInterceptor) {
         RestInterceptorRegistration registration = new RestInterceptorRegistration(restInterceptor);
         registrations.add(registration);
         return registration;
     }
 
+    /**
+     * Reflects the registrations in the registry. This method should be called after all the registrations are done.
+     * <p>Will be deprecated in the future.
+     */
     public void build() {
         this.registrations.forEach(registration -> {
             RestInterceptor restInterceptor = registration.getRestInterceptor();
