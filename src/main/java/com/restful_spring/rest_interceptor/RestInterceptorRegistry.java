d88a19849972a2d0d5a151ceb7ffed5a7234f9f4
KimDaehyeon <daehyeon3351@gmail.com>
2024-12-22 06:19:27 +0900

    refactor: Remove build method - #13
    - build() method can cause inconvenience to users.
    - And it cannot guarantee that the build() method was called at compile time.

--- src/main/java/com/restful_spring/rest_interceptor/RestInterceptorRegistry.java
+++ src/main/java/com/restful_spring/rest_interceptor/RestInterceptorRegistry.java
@@ -1,8 +1,5 @@
 package com.restful_spring.rest_interceptor;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 /**
@@ -14,7 +11,6 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 public class RestInterceptorRegistry {
 
     private final InterceptorRegistry registry;
-    private final List<RestInterceptorRegistration> registrations = new ArrayList<>();
 
     public RestInterceptorRegistry(InterceptorRegistry registry) {
         this.registry = registry;
@@ -28,24 +24,6 @@ public class RestInterceptorRegistry {
      * restInterceptor further for example adding RestfulPatterns it should apply to.
      */
     public RestInterceptorRegistration addInterceptor(RestInterceptor restInterceptor) {
-        RestInterceptorRegistration registration = new RestInterceptorRegistration(restInterceptor);
-        registrations.add(registration);
-        return registration;
-    }
-
-    /**
-     * Reflects the registrations in the registry. This method should be called after all the registrations are done.
-     * <p>Will be deprecated in the future.
-     */
-    public void build() {
-        this.registrations.forEach(registration -> {
-            RestInterceptor restInterceptor = registration.getRestInterceptor();
-
-            registry.addInterceptor(restInterceptor)
-                    .addPathPatterns(restInterceptor.restfulPatterns.stream()
-                            .map(RestfulPattern::getPath)
-                            .collect(Collectors.toList()))
-                    .order(registration.getOrder());
-        });
+        return new RestInterceptorRegistration(restInterceptor, registry);
     }
 }
